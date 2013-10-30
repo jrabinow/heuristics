daddf4c982f5e0266e3e3f07782aa65d240f2d61
Eric Schles <ericschles@gmail.com>
2013-10-29 22:29:40 -0400

    all the things

---
+++ nanomunch/Eric_nano.java
@@ -0,0 +1,66 @@
+
+class Graph {
+
+ 
+    public List<Node> nodes = new ArrayList();
+
+
+    public Graph() 
+    {
+    
+    }
+
+
+	
+}
+
+class Node {
+
+    public 
+
+
+}
+
+
+class NanoMuncher {
+
+
+}
+
+public class Eric_nano {
+    
+
+    public Graph DoMoveOne(Graph graph) {
+	
+    
+    }
+
+    public Graph DoMoveTwo(Graph graph) {
+	    
+    }
+
+    public Graph DoMoveThree(Graph graph) {
+    
+
+    }
+    
+    public Graph DoMoveFour(Graph graph) {
+
+    }
+
+
+    public static void main(String[] Args) {
+	
+	int counter = 0;
+	int NodesLeft;
+	Graph graph = new Graph();
+	while(NodesLeft > 0) {
+	    
+	    graph.getNextState(nanoMunchers)
+
+	}
+	
+
+
+    }
+}
